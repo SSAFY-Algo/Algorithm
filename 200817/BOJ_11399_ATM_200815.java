567c9c67ec0162027dbbc3591883c7c6a5c5de5d
username <34119701+ShinDajeong@users.noreply.github.com>
2020-08-15 22:21:06 +0900

    200815 BOJ 11399 ATM[S3]

---
+++ 200817/BOJ_11399_ATM_200815.java
@@ -0,0 +1,28 @@
+package com.ssafy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_11399_ATM_200815 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		int result = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<=i; j++) {
+				result += arr[j];
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
