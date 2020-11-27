f2503978776049018fb0fda71b4af9a8e22a0e21
rhdrlvnd <40141212+rhdrlvnd@users.noreply.github.com>
2020-11-28 03:36:06 +0900

    Update SWEA_2117_홈방범서비스_201128.java

--- 201128/Dawoon/SWEA_2117_홈방범서비스_201128.java
+++ 201128/Dawoon/SWEA_2117_홈방범서비스_201128.java
@@ -34,9 +34,7 @@ public class SWEA_2117_홈방범서비스_201128 {
 			ansHouse = 0;
 			for (int i = 0; i < map.length; i++) {
 				for (int j = 0; j < map.length; j++) {
-					for (int l = 1; l <= K; l++) {
-						play(i, j, l, M, map);
-					}
+					play(i, j, K, M, map);
 				}
 			}
 			System.out.println("#" + (tc + 1) + " " + ansHouse);
@@ -46,31 +44,39 @@ public class SWEA_2117_홈방범서비스_201128 {
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
-	private static void play(int x, int y, int n, int M, int[][] map) {
+	private static void play(int x, int y, int K, int M, int[][] map) {
 		LinkedList<int[]> q = new LinkedList<int[]>();
 		boolean[][] visit = new boolean[map.length][map.length];
 		int cntHouse = 0;
 
 		// x, y, cnt
 		int[] start = { x, y, 1 };
+		int n = 1;
 		q.offer(start);
-		if(map[x][y] == 1)
-			cntHouse++;
 		visit[x][y] = true;
 		
 		while (!q.isEmpty()) {
 			int[] now = q.poll();
-			if (now[2] == n)
+			if (now[2] == K)
 				continue;
 
+			if(n != now[2]) {
+				int companyCost = (n * n) + ((n - 1) * (n - 1));
+				int houseCost = cntHouse * M;
+				if (houseCost - companyCost >= 0 && cntHouse > ansHouse) {
+					ans = companyCost - houseCost;
+					ansHouse = cntHouse;
+				}
+				n++;
+			}
+			if (map[now[0]][now[1]] == 1)
+				cntHouse++;
 			for (int i = 0; i < dr.length; i++) {
 				int nx = now[0] + dr[i];
 				int ny = now[1] + dc[i];
 				if (nx < 0 || ny < 0 || nx >= map.length || ny >= map.length)
 					continue;
 				if (!visit[nx][ny]) {
-					if (map[nx][ny] == 1)
-						cntHouse++;
 					int[] p = { nx, ny, now[2] + 1 };
 					q.offer(p);
 					visit[nx][ny] = true;
@@ -80,7 +86,7 @@ public class SWEA_2117_홈방범서비스_201128 {
 
 		int companyCost = (n * n) + ((n - 1) * (n - 1));
 		int houseCost = cntHouse * M;
-		if (houseCost - companyCost > 0 && cntHouse > ansHouse) {
+		if (houseCost - companyCost >= 0 && cntHouse > ansHouse) {
 			ans = companyCost - houseCost;
 			ansHouse = cntHouse;
 		}
