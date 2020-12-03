6a34edbc1eadc2d760eed48a564332cebdbb5e88
rhdrlvnd <rhdrlvnd@gmail.com>
2020-12-03 20:44:43 +0900

    201129 BOJ 16194 카드구매하기2 [S1]

---
+++ 201203/Dawoon/BOJ_16194_카드구매하기2_201129.java
@@ -0,0 +1,34 @@
+package com.ssafy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BOJ_16194_카드구매하기2_201129 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] card = new int[N+1];
+		int[] dp = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			dp[i] = card[i];
+			for (int j = 1; j < i; j++) {
+				dp[i] = Math.min(dp[i], card[j] + dp[i-j]);
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
