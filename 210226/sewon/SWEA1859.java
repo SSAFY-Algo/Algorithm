f86edf721ab943affbb67d54ac4bca4e9367b812
sewonkimm <swon962@gmail.com>
2021-02-27 20:33:49 +0900

    1859 수정

--- 210226/sewon/SWEA1859.java
+++ 210226/sewon/SWEA1859.java
@@ -3,39 +3,31 @@ import java.util.*;
 
 class Solution
 {
-	static int N;
-	static int[] price;
-	static long answer;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int testcase = 1; testcase <= T; testcase++) {
-			answer = Integer.MIN_VALUE;
-			N = Integer.parseInt(br.readLine());
-			price = new int[N];
+			long answer = 0;
+			int N = Integer.parseInt(br.readLine());
+			int[] price = new int[N];
+			int pivot = N-1;	// 현재 가리키고 있는 값(맨 뒤부터 시작)
 			
-			StringTokenizer st = new StringTokenizer(br.readLine(), &quot; &quot;);
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			for (int i = 0; i < N; i++) {
 				price[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			calc(1, 0-price[0], 1);	// 첫번째 날 사는 경우
-			calc(1, 0, 0); 			// 첫번째 날 안사는 경우
+			for (int i = N-2; i >= 0; i--) {
+				if(price[i] < price[pivot]) {
+					answer += price[pivot] - price[i];
+				}
+				else {
+					pivot = i;
+				}
+			}
 			
-			System.out.println(&quot;#&quot; + testcase + &quot; &quot; + answer);
+			System.out.println("#" + testcase + " " + answer);
 		}
 	}
-	
-	static public void calc(int day, long sum, int cnt) {
-		
-		if(day == N) {
-			answer = Math.max(answer, sum);
-			return;
-		}
-		
-		calc(day+1, sum-price[day], cnt+1); // case 1 : 산다
-		calc(day+1, sum, cnt); // case 2 : 안산다
-		calc(day+1, sum+(price[day]*cnt), 0); // case 3 : 판다
-	} 
 }
